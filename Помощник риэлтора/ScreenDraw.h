0664eae0a9d2a1d1d68d95485137cc92c8801887
Michael <polkandra@gmail.com>
2017-03-20 00:05:09 +0300

    check commit

---
+++ Помощник риэлтора/ScreenDraw.h
@@ -0,0 +1,13 @@
+//
+//  ScreenDraw.h
+//  Помощник риэлтора
+//
+//  Created by Mikhail Kozlyukov on 19.03.17.
+//  Copyright © 2017 Chebahatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenDraw : UIView
+
+@end
