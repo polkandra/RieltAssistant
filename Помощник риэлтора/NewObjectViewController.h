808b4ac7a015206f9bd75afb9380050f2a14a35d
Michael <polkandra@gmail.com>
2017-03-27 20:41:34 +0300

    added outlets to new VC

--- Помощник риэлтора/NewObjectViewController.h
+++ Помощник риэлтора/NewObjectViewController.h
@@ -8,6 +8,49 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NewObjectViewController : UIViewController
+@interface NewObjectViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UITextField *adressTextfield;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *mapAddonLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *roomAddonLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalSquareLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *totalSquareTextField;
+@property (weak, nonatomic) IBOutlet UILabel *totalSquareMetersLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *livingSquareLabel;
+@property (weak, nonatomic) IBOutlet UITextField *livingSquareTextField;
+@property (weak, nonatomic) IBOutlet UILabel *livingSquareMetersLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *kitchenSquareLabel;
+@property (weak, nonatomic) IBOutlet UITextField *kitchenSquareTextField;
+@property (weak, nonatomic) IBOutlet UILabel *kitchenMetersLabel;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UILabel *priceRoublesLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *metroFloorLabel;
+@property (weak, nonatomic) IBOutlet UITextField *metroFloorTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *addPhotosLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *ownerNameLabel;
+@property (weak, nonatomic) IBOutlet UITextField *ownerNameTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *extraInfoLabel;
+@property (weak, nonatomic) IBOutlet UITextField *extraInfoTextField;
+
+
 
 @end
