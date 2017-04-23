355513bcd3d05282978f659f448993182d67f2ab
Michael <polkandra@gmail.com>
2017-04-23 23:11:21 +0300

    add detailVC

---
+++ Помощник риэлтора/DetailObjectController.h
@@ -0,0 +1,13 @@
+//
+//  DetailObjectController.h
+//  RieltAssistant
+//
+//  Created by Mikhail Kozlyukov on 23.04.17.
+//  Copyright © 2017 Chebahatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailObjectController : UITableViewController
+
+@end
