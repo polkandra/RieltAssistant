1360382efcb869033764acfbb686888eadba1471
Michael <polkandra@gmail.com>
2017-12-12 19:48:37 +0300

    Splash screen icons added, icons for app added

---
+++ Помощник риэлтора/SplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenViewController.h
+//  RieltAssistant
+//
+//  Created by Mikhail Kozlyukov on 12.12.2017.
+//  Copyright © 2017 Chebahatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+@end
---
+++ Помощник риэлтора/SplashScreenViewController.m
@@ -0,0 +1,29 @@
+//
+//  SplashScreenViewController.m
+//  RieltAssistant
+//
+//  Created by Mikhail Kozlyukov on 12.12.2017.
+//  Copyright © 2017 Chebahatt. All rights reserved.
+//
+
+#import "SplashScreenViewController.h"
+
+@interface SplashScreenViewController ()
+
+@end
+
+@implementation SplashScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    [self performSelector:@selector(showNavController) withObject:nil afterDelay:3];
+}
+
+-(void) showNavController {
+    
+    [self performSegueWithIdentifier:@"fromSplashScreen" sender:self];
+}
+
+@end
